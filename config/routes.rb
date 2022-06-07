9c0a8495d608e82123a0b54730d73069269925e4
daudi13 <davouma13@gmail.com>
2022-06-07 18:07:27 +0300

    Add end

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'users#index'
   resources :users, only: [:index, :show] do
-    resources :post, only: [:index, :show]
+    resources :posts, only: [:index, :show]
+  end
 end
