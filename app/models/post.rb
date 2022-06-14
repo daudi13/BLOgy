272f849254b39df8ddf8f64997aa8ed654b25292
daudi13 <davouma13@gmail.com>
2022-06-14 13:24:38 +0300

    Add recent_comments method in Post class

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   after_save :comments_counter
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
