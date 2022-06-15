c990172fe4b8464b65c79bb0d2594f06e06aacc1
daudi13 <davouma13@gmail.com>
2022-06-15 16:55:35 +0300

    In this file I: - Added a variable for inside the method index - Added a varibale inside show method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = current_user
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+  end
 end
