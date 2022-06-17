6d1ada003e15dbfeab3541526ecee3a9d87e83b1
daudi13 <davouma13@gmail.com>
2022-06-17 18:39:59 +0300

    Add test for comments_counter

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   before(:all) do
     @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
     @post = Post.create(title: 'Hello', author:@user)
