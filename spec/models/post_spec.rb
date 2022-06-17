b089a0d25683b29688160c18e7fc0dbddee96264
daudi13 <davouma13@gmail.com>
2022-06-17 19:00:43 +0300

    Add test for like_counter

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:all) do
+    @user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+    @post = Post.create(title: 'Hello', author:@user) 
+  end
+
+  it 'should return comments_counter greater than or equal to 0' do
+    @post.comments_counter = -15
+    expect(@post).to_not be_valid
+
+    @post.comments_counter = 0
+    expect(@post).to be_valid
+
+    @post.comments_counter = 16
+    expect(@post).to be_valid
+  end
+
+  it 'should have likes_counter numericaly' do
+    @post.likes_counter = 'one'
+    expect(@post).to_not be_valid
+
+    @post.likes_counter = 20
+    expect(@post).to be_valid
+  end
 end
