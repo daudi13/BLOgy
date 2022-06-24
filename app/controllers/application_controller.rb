f52aa716c0dffaa68e4064c950538a750f7c7865
daudi13 <davouma13@gmail.com>
2022-06-24 11:43:57 +0300

    fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,17 +13,4 @@ class ApplicationController < ActionController::Base
       keys: %i[name email bio password password_confirmation]
     )
   end
-
-  def authorize_request
-  header = request.headers['Authorization']
-  header = header.split.last if header
-  begin
-    @decoded = JsonWebToken.decode(header)
-    p @decoded
-    @current_user = User.find(@decoded['id'])
-  rescue ActiveRecord::RecordNotFound => e
-    render json: { errors: e.message }, status: :unauthorized
-  rescue JWT::DecodeError => e
-    render json: { error: e.message }, status: :unauthorized
-  end
 end
