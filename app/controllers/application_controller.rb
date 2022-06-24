ffbb1751e414791f008d62e1f6061563402a1957
daudi13 <davouma13@gmail.com>
2022-06-24 09:24:09 +0300

    configure ApplicationController with Api methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  include Response
+  include ExceptionHandler
+  protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -8,5 +12,18 @@ class ApplicationController < ActionController::Base
       :sign_up,
       keys: %i[name email bio password password_confirmation]
     )
+  end
+
+  def authorize_request
+  header = request.headers['Authorization']
+  header = header.split.last if header
+  begin
+    @decoded = JsonWebToken.decode(header)
+    p @decoded
+    @current_user = User.find(@decoded['id'])
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { errors: e.message }, status: :unauthorized
+  rescue JWT::DecodeError => e
+    render json: { error: e.message }, status: :unauthorized
   end
 end
