5938a232b4e08a2cf56d6ebe3d5da4935615379c
daudi13 <davouma13@gmail.com>
2022-06-24 11:55:45 +0300

    fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   def authorize_request
     header = request.headers['Authorization']
     header = header.split.last if header
