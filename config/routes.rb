adaba5125682493850cfdc02ebc62f3f98385870
Zahra Arshia <fzarshia@gmail.com>
2022-06-24 13:22:41 +0430

    Update routes.rb
    add API routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,12 @@ Rails.application.routes.draw do
       resources :likes, only: [:new, :create]
     end
   end
+  
+  namespace :api do
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index] do
+        resources :comments, only: [:index]
+      end
+    end
+  end
 end
